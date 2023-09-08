73988d8ab49443c0b3171803a5b64c2c480af91c
Federico Calicchia <federicokalik@gmail.com>
2023-09-08 16:11:05 +0200

    Aggiornamenti generali
    Aggiornato Astro alla versione 3.0.12
    Rimosso temporaneamente lo switch per la modalità scura
    Aggiunte transizioni di pagina
    Fix generali

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
+import prefetch from '@astrojs/prefetch';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // ...
+  integrations: [
+    prefetch({
+      // Only prefetch links with an href that begins with `/work/`
+      selector: "a[href^='/work/']",
+    }),
+  ],
+});
