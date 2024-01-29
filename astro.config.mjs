90841a1d5cb00359f8304bc38fb7bad1c4bb88a8
Federico Calicchia <52151944+Federicokalik@users.noreply.github.com>
2024-01-29 14:46:47 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from 'astro/config';
-import prefetch from '@astrojs/prefetch';
 
+// https://astro.build/config
 export default defineConfig({
-  // ...
-  integrations: [
-    prefetch({
-      // Only prefetch links with an href that begins with `/work/`
-      selector: "a[href^='/work/']",
-    }),
-  ],
+  prefetch: true,
+  experimental: {
+    clientPrerender: true,
+  },
 });
